1cbf44dfd293d77dc907bd8c8bb13e2a0feb9b01
sabbir-veer <sabbir.hstu.19@gmail.com>
2026-01-11 23:45:47 +0600

    Feat: initializing portfolio

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import { navIcons, navLinks } from '#constants'
+import dayjs from 'dayjs'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src='/images/logo.svg' alt='logo' />
+        <p className='font-bold'>Sabbir's Portfolio</p>
+        <ul>
+          {navLinks.map(({ id, name }) => (
+            <p key={id}>{name}</p>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img src={img} alt={`icon-${id}`} />
+            </li>
+          ))}
+        </ul>
+        <time>{dayjs().format('ddd MMM D h:mm A')}</time>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '#components': resolve(__dirname, 'src/components'),
+      '#constants': resolve(__dirname, 'src/constants'),
+      '#store': resolve(__dirname, 'src/store'),
+      '#hoc': resolve(__dirname, 'src/hoc'),
+      '#windows': resolve(__dirname, 'src/windows'),
+    },
+  },
+})
